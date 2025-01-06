501b70b7288e182dcbc3a692e730779ad0ec6d0e
rajesh <rajeshsharmax2c@gmail.com>
2025-01-06 14:48:40 +0530

    initial commmit

---
+++ gateway/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/rajeshsharmax2c/tolling/aggregator/client"
+	"github.com/sirupsen/logrus"
+)
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+func main() {
+	listenAddr := flag.String("listenAddr", ":6000", "the listen address of the HTTP server")
+	aggregatorServiceAddr := flag.String("aggServiceAddr", "http://localhost:4000", "the listen address of the aggregator service")
+	flag.Parse()
+	var (
+		client     = client.NewHTTPClient(*aggregatorServiceAddr) // endpoint of the aggregator service
+		invHandler = newInvoiceHandler(client)
+	)
+	http.HandleFunc("/invoice", makeAPIFunc(invHandler.handleGetInvoice))
+	logrus.Infof("gateway HTTP server running on port %s", *listenAddr)
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
+
+type InvoiceHandler struct {
+	client client.Client
+}
+
+func newInvoiceHandler(c client.Client) *InvoiceHandler {
+	return &InvoiceHandler{client: c}
+}
+
+func (h *InvoiceHandler) handleGetInvoice(w http.ResponseWriter, r *http.Request) error {
+	fmt.Println("htting the get invoice from the gateway")
+	values, ok := r.URL.Query()["obu"]
+	if !ok || len(values[0]) < 1 {
+		return fmt.Errorf("obu id is missing")
+	}
+
+	obuID, err := strconv.Atoi(values[0])
+	if err != nil {
+		return fmt.Errorf("invalid OBU id")
+	}
+
+	// Pass the obuID to the GetInvoice method
+	inv, err := h.client.GetInvoice(context.Background(), obuID)
+	if err != nil {
+		return err
+	}
+	return writeJSON(w, http.StatusOK, inv)
+
+}
+
+func writeJSON(w http.ResponseWriter, code int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeAPIFunc(fn apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			logrus.WithFields(logrus.Fields{
+				"took": time.Since(start),
+				"uri":  r.RequestURI,
+			}).Info("REQ :: ")
+		}(time.Now())
+		if err := fn(w, r); err != nil {
+			writeJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+	}
+}
